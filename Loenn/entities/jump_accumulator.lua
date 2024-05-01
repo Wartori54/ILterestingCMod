59b1c0f4f44d8a069f4a10a09057e8f469c3a9f1
Wartori54 <wartori54@gmail.com>
2024-05-01 17:30:46 +0200

    Add jumpAccumulator

---
+++ Loenn/entities/jump_accumulator.lua
@@ -0,0 +1,14 @@
+local jump_accumulator = {
+    name = "ILterestingCMod/JumpAccumulatorEntity",
+    depth = -8500,
+    -- texture = "objects/ILterestingCMod/dashfullarea/idle00", 
+    placements = {
+        {
+            name = "normal",
+            data = {
+                size = 64
+            }
+        }
+    },
+};
+return jump_accumulator;
